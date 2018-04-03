ee0c6c3e35351da2ba91976aac5c0c186adcf32d
vadym <akademiya.adw@gmail.com>
2018-04-03 18:24:50 +0300

    add edit person on list kido

--- app/src/main/java/com/example/user/bestfriends/list_kido/adapter/PersonAdapter.java
+++ app/src/main/java/com/example/user/bestfriends/list_kido/adapter/PersonAdapter.java
@@ -2,11 +2,16 @@ package com.example.user.bestfriends.list_kido.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.user.bestfriends.R;
 import com.example.user.bestfriends.list_kido.Person;
@@ -38,14 +43,53 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonHolder> {
         final Person singlePerson = personList.get(position);
 
         holder.personName.setText(singlePerson.getPersonName());
+        holder.edit_item.setOnClickListener(v -> editTaskDialog(singlePerson));
+        holder.listView.setOnLongClickListener(v -> {
+            holder.listReview.setVisibility(View.VISIBLE);
+            return false;
+        });
         holder.deleteItem.setOnClickListener(v -> {
             database.deleteItem(singlePerson.getPersonID());
             ((Activity)context).finish();
             context.startActivity(((Activity) context).getIntent());
         });
+        holder.go_back.setOnClickListener(v -> holder.listReview.setVisibility(View.GONE));
     }
-
 
     @Override
     public int getItemCount() { return personList.size(); }
+
+    private void editTaskDialog(final Person person){
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View subView = inflater.inflate(R.layout.item_edit_list_kido, null);
+
+        final EditText nameField = subView.findViewById(R.id.create_person_name);
+
+        if(person != null){
+            nameField.setText(person.getPersonName());
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.edit_person);
+        builder.setView(subView);
+        builder.create();
+
+        builder.setPositiveButton(R.string.edit_person, (dialog, which) -> {
+            final String name = nameField.getText().toString();
+
+            if(TextUtils.isEmpty(name)){
+                Toast.makeText(context, R.string.something_wrong, Toast.LENGTH_SHORT).show();
+            }
+            else{
+                assert person != null;
+                database.updatePerson(new Person(person.getPersonID(), name));
+                ((Activity)context).finish();
+                context.startActivity(((Activity)context).getIntent());
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, (dialog, which) ->
+                Toast.makeText(context, R.string.task_cancelled, Toast.LENGTH_SHORT).show());
+        builder.show();
+    }
 }
--- app/src/main/java/com/example/user/bestfriends/list_kido/adapter/PersonHolder.java
+++ app/src/main/java/com/example/user/bestfriends/list_kido/adapter/PersonHolder.java
@@ -3,6 +3,7 @@ package com.example.user.bestfriends.list_kido.adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -12,12 +13,20 @@ import com.example.user.bestfriends.R;
 public class PersonHolder extends RecyclerView.ViewHolder {
     TextView personName;
     ImageView deleteItem;
+    ImageView edit_item;
+    ImageView go_back;
     CheckBox check_people;
+    RelativeLayout listView;
+    FrameLayout listReview;
 
     public PersonHolder(View view) {
         super(view);
         personName = view.findViewById(R.id.person_name);
         deleteItem = view.findViewById(R.id.delete_item);
+        edit_item = view.findViewById(R.id.edit_item);
+        go_back = view.findViewById(R.id.go_back);
         check_people = view.findViewById(R.id.check_people);
+        listView = view.findViewById(R.id.listView);
+        listReview = view.findViewById(R.id.listReview);
     }
 }
